3d0bdf581493844a044dd17e1c12fa017121bf14
abhishekydv7 <abhibuddy224@gmail.com>
2026-01-02 17:35:59 +0000

    update

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,12 +1,29 @@
 
-function sayMyName() {
-    console.log("H")
-    console.log("I")
-    console.log("T")
-    console.log("E")
-    console.log("S")
-    console.log("H")
-    console.log(", here's my name")
+// function sayMyName() {
+//     console.log("H")
+//     console.log("I")
+//     console.log("T")
+//     console.log("E")
+//     console.log("S")
+//     console.log("H")
+//     console.log(", here's my name")
+// }
+
+//sayMyName()
+
+function addTwoNumbers(number1, number2){
+    
+   // let result = number1 + number2
+   // return result
+    return number1 + number2
 }
 
-sayMyName()
+const result = addTwoNumbers(5,3)
+
+//console.log("Result: ", result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("hitesh"))
