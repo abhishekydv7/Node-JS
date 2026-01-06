0a4dcc400ec5ef367651480a7080679bcdfc3ad0
abhishekydv7 <abhibuddy224@gmail.com>
2026-01-06 14:08:11 +0000

    arrowdone

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -11,11 +11,27 @@ const user = {
 
 //user.welcomeMessage()
 
-const chill = function(){
+// const chill = function(){
+//     let username = "jimbei"
+//     console.log(this.username);
+// }
+const chill = () => {
     let username = "jimbei"
     console.log(this.username);
 }
 
-chill()
 
-//skip for today
+
+
+//chill()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+//const addTwo = (num1, num2) => num1 + num2   
+const addTwo = (num1, num2) => (num1 + num2)   // if there are curly braces a return should be mentioned and if () brackets applied, return isn't necessary
+ 
+console.log(addTwo(2, 3));
+
+//5:47
+
