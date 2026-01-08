356dcc799ffb8c6c5f2b1ff44d35d66b7e6a3145
abhishekydv7 <abhibuddy224@gmail.com>
2026-01-08 20:17:02 +0000

    control

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+//if
+const isUserLoggedIn = true
+const temperature = 41
+
+if( temperature === 41 ) {
+    console.log("less than 50");
+} else {
+    console.log("temp is greater than 50");
+}
+// <, > , <= , >=, ==, !=, ===,!==
