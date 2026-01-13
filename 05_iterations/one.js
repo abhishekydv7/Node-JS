46dac03acc48bfeebfe634414259d7b2e889d94d
abhishekydv7 <abhibuddy224@gmail.com>
2026-01-13 16:49:11 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,32 @@
+// for 
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let i =0; i<= 10; i++){
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+for(let i = 0; i<= 10; i++) {
+   // console.log(`Outer loop value: ${i}`);
+    for (let j= 0; j<= 10; j++){
+        //console.log(`Inner loop value ${j} and inner loop ${i}`);
+       // console.log(i + '*' + j + '=' + i * j);
+        
+    }
+}
+
+let myArray = ['luffy','zoro','nami']
+console.log(myArray.length);
+for(let i = 0; i < myArray.length; i++){
+    const element = myArray[i];
+    console.log(element);
+    
+}
+
+
+//break and continue -- easy read notes
