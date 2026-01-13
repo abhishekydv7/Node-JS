38747879c45320aa8c18fcc67ae5a063b16fd728
abhishekydv7 <abhibuddy224@gmail.com>
2026-01-13 19:52:41 +0000

    forin_loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,13 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+//8:06:00
--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,14 +1,23 @@
-let i =0
-while (i <= 10) {
-    console.log(`Value of index is ${i}`);
-    i = i + 2
-}
+//              while loop
+// let i =0
+// while (i <= 10) {
+//     console.log(`Value of index is ${i}`);
+//     i = i + 2
+// }
 
 let myArray = ['luffy', 'zoro', 'nami']
 
 let arr = 0
 while (arr< myArray.length) {
-    console.log(`Value is ${myArray[arr]}`);
-    arr =arr +1
+    console.log(`Hero's are ${myArray[arr]}`);
+    arr = arr +1
     
 }
+
+let score =1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <= 10)
