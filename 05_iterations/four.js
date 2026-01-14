6d24502b0a75dbf60709fc176f3e9934b5b25d3e
abhishekydv7 <abhibuddy224@gmail.com>
2026-01-14 20:00:54 +0000

    for_of

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,13 +1,26 @@
-const myObject = {
-    js: 'javascript',
-    cpp: 'C++',
-    rb: "ruby",
-    swift: "swift by apple"
-}
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
 
-for (const key in myObject) {
-    console.log(`${key} shortcut is for ${myObject[key]}`);
+// for (const key in myObject) {
+//   //  console.log(`${key} shortcut is for ${myObject[key]}`);
     
-}
+// }
 
-//8:06:00
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//    // console.log(programming[key]);
+// }
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+
+// for(const key in map) {
+//     console.log(key);
+    
+// }
