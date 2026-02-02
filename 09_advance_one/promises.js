a803a464faef61e431bc74d0bebd60f8418fbc88
mrx <gettingpsyched.23@gmail.com>
2026-02-02 23:10:47 +0530

    promises pt 2

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -64,3 +64,39 @@ promiseFour
 .finally(()=> console.log("The Promise is either resolved or rejected")
 )
 
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true
+        if(!error) {
+            resolve({username: "javascript", password:"2312"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+})
+
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    } catch (error){
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//    try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//    } catch (error) {
+//         console.log("E:", error); 
+//    }
+// }
+//getAllUsers()
+
+fetch ('https://jsonplaceholder.typicode.com/users')
+.then9
+//6:15
