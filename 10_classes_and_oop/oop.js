2b16b7bd168aed189ec47449331d870a8a511135
abhishekydv7 <abhibuddy224@gmail.com>
2026-02-04 22:36:40 +0530

    oop pt1(a)

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -12,7 +12,28 @@ const user = {
     }
 }
 
-console.log(user.username);
+//console.log(user.username);
 //console.log(user.getUserDetails());
-console.log(this);
+//console.log(this);
 
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting =function(){
+        console.log(`Welcome ${this.username}`);
+        
+    }
+    return this
+}
+
+const  userOne = new User("hitesh", 12, true)
+const  userTwo = new User("ChaiAurCode", 11, false)
+
+console.log(userOne.constructor);
+//console.log(userTwo);
